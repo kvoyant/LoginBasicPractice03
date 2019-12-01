8d64d1818a4c24483742c5287f829c32e1191f8e
kvoyant <kvoyant@gmail.com>
2019-12-01 14:56:45 +0900

    캐스팅에러 테스트 - Logcat

--- app/src/main/java/com/yhkim/loginbasicpractice03/UserInfoActivity.kt
+++ app/src/main/java/com/yhkim/loginbasicpractice03/UserInfoActivity.kt
@@ -11,17 +11,22 @@ class UserInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
 
-//        val userIdFromIntent = intent.getStringExtra("userId")
+        val userIdFromIntent = intent.getStringExtra("userId")
 //        idTxt.text = userIdFromIntent
-//        val userPw = intent.getStringExtra("userPw")
+        val userPw = intent.getStringExtra("userPw")
 //        pwTxt.text = userPw
-//        var userHeight = intent.getDoubleExtra("userHeight" , 0.1)
+        var userHeight = intent.getDoubleExtra("userHeight" , 0.1)
+
+/*
+//Logcat 에서 에러 확인 ( 형변화 불가 )
+        var userHeight = intent.getDoubleExtra("userHeight" , 0.1) as Int
+//        Caused by: java.lang.ClassCastException: java.lang.Double cannot be cast to java.lang.Integer
+//        at com.yhkim.loginbasicpractice03.UserInfoActivity.onCreate(UserInfoActivity.kt:18)
+*/
 
         val user = intent.getSerializableExtra("userData") as UserData //캐스팅 swift 유사
         idTxt.text = user.userLoginId
         pwTxt.text = user.userLoginPw
 
-
     }
 }
-//인텐트로 데이터 이동시
